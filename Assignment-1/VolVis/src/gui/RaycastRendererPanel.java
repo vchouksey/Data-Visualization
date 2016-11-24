202b48df5ae3a87f55058d6f7164946c3dd0baa8
TimothyAerts <t.g.aerts@student.tue.nl>
2016-11-24 15:32:44 +0100

    Trilinear, MIP , start of composite
    MIP working, trilinear working and checked(with benjamin). MIP carefull
    with max needs to be double. COmposite needs the multiplication and
    summation function still.

--- Assignment-1/VolVis/src/gui/RaycastRendererPanel.java
+++ Assignment-1/VolVis/src/gui/RaycastRendererPanel.java
@@ -133,15 +133,15 @@ public class RaycastRendererPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void mipButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mipButtonActionPerformed
-        JOptionPane.showMessageDialog(this, "Not implemented.");
+        renderer.setMode(1);
     }//GEN-LAST:event_mipButtonActionPerformed
 
     private void slicerButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_slicerButtonActionPerformed
-        
+        renderer.setMode(0);
     }//GEN-LAST:event_slicerButtonActionPerformed
 
     private void compositingButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_compositingButtonActionPerformed
-        JOptionPane.showMessageDialog(this, "Not implemented.");
+        //renderer.setMode(2);
     }//GEN-LAST:event_compositingButtonActionPerformed
 
     private void tf2dButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf2dButtonActionPerformed
